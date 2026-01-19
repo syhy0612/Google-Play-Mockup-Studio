f5f6c1d44cc4dcaf0a17bea52d8e4b0ccfcf7846
syhy <syhy0612@gmail.com>
2026-01-19 16:04:37 +0800

    Fix android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.google.Mockup',
+  appName: 'Google Play',
+  webDir: 'dist'
+};
+
+export default config;
