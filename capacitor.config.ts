19affa3c16eb660928958b34de318e5bf703f1a2
syhy <syhy0612@gmail.com>
2026-01-20 01:17:23 +0800

    chore: 更新应用名称为 Play Mockup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.google.mockup',
-  appName: 'Google Play',
+  appName: 'Play Mockup',
   webDir: 'dist',
   
   // ✨ 新增这部分配置 ✨
